35fb5dc9c52709ded01dc53f56fc37dd53103675
Crazy_Hourse <kadi741@mail.ru>
2017-03-06 23:09:07 -0800

    Add files via upload

---
+++ FakeFPS/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace FakeFPS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(93, 411);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(30, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "820";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(123, 414);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(10, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "-";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(133, 411);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(30, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "925";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(169, 411);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(68, 20);
+            this.textBox3.TabIndex = 1;
+            this.textBox3.Text = "Повторений";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 408);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Старт";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(243, 411);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(86, 20);
+            this.textBox4.TabIndex = 1;
+            this.textBox4.Text = "Скорость(в мс)";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(305, 146);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(56, 88);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(243, 146);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(56, 88);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(181, 146);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(56, 88);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(2, 375);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(570, 27);
+            this.panel1.TabIndex = 4;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(151, 146);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(24, 88);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lime;
+            this.ClientSize = new System.Drawing.Size(575, 437);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+    }
+}
+
---
+++ FakeFPS/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+
+namespace FakeFPS
+{
+    public partial class Form1 : Form
+    {
+        public int a, i, b, count, fps;
+        public string fpsS;
+        public char thousand, hundred, ten, one;
+        public Random rand = new Random();
+        public ushort ExplorerCount=2;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox3.Text == "Повторений" || textBox4.Text == "Скорость(в мс)") { MessageBox.Show("Даун введи скорость и количество повторений."); textBox3.Text = Convert.ToString(100); textBox4.Text = Convert.ToString(100); }
+            else
+            {
+                a = Convert.ToInt32(textBox1.Text);
+                b = Convert.ToInt32(textBox2.Text);
+                count = Convert.ToInt32(textBox3.Text);
+                timer1.Interval = Convert.ToInt32(textBox4.Text);
+                panel1.Location = new Point(2, 405);
+                timer1.Enabled = true;
+            }
+        }
+        public void tenMethod(int i)
+        {
+            switch(i)
+            {
+                case 0:
+                    pictureBox2.Image = FakeFPS.Properties.Resources._0;
+                    break;
+                case 1:
+                    pictureBox2.Image = FakeFPS.Properties.Resources._1;
+                    break;
+                case 2:
+                    pictureBox2.Image = FakeFPS.Properties.Resources._2;
+                    break;
+                case 3:
+                    pictureBox2.Image = FakeFPS.Properties.Resources._3;
+                    break;
+                case 4:
+                    pictureBox2.Image = FakeFPS.Properties.Resources._4;
+                    break;
+                case 5:
+                    pictureBox2.Image = FakeFPS.Properties.Resources._5;
+                    break;
+                case 6:
+                    pictureBox2.Image = FakeFPS.Properties.Resources._6;
+                    break;
+                case 7:
+                    pictureBox2.Image = FakeFPS.Properties.Resources._7;
+                    break;
+                case 8:
+                    pictureBox2.Image = FakeFPS.Properties.Resources._8;
+                    break;
+                case 9:
+                    pictureBox2.Image = FakeFPS.Properties.Resources._9;
+                    break;
+            }
+        }
+        public void oneMethod(int i)
+        {
+            switch (i)
+            {
+                case 0:
+                    pictureBox3.Image = FakeFPS.Properties.Resources._0;
+                    break;
+                case 1:
+                    pictureBox3.Image = FakeFPS.Properties.Resources._1;
+                    break;
+                case 2:
+                    pictureBox3.Image = FakeFPS.Properties.Resources._2;
+                    break;
+                case 3:
+                    pictureBox3.Image = FakeFPS.Properties.Resources._3;
+                    break;
+                case 4:
+                    pictureBox3.Image = FakeFPS.Properties.Resources._4;
+                    break;
+                case 5:
+                    pictureBox3.Image = FakeFPS.Properties.Resources._5;
+                    break;
+                case 6:
+                    pictureBox3.Image = FakeFPS.Properties.Resources._6;
+                    break;
+                case 7:
+                    pictureBox3.Image = FakeFPS.Properties.Resources._7;
+                    break;
+                case 8:
+                    pictureBox3.Image = FakeFPS.Properties.Resources._8;
+                    break;
+                case 9:
+                    pictureBox3.Image = FakeFPS.Properties.Resources._9;
+                    break;
+            }
+        }
+        public void hundredMethod(int i)
+        {
+            switch (i)
+            {
+                case 0:
+                    pictureBox1.Image = FakeFPS.Properties.Resources._0;
+                    break;
+                case 1:
+                    pictureBox1.Image = FakeFPS.Properties.Resources._1;
+                    break;
+                case 2:
+                    pictureBox1.Image = FakeFPS.Properties.Resources._2;
+                    break;
+                case 3:
+                    pictureBox1.Image = FakeFPS.Properties.Resources._3;
+                    break;
+                case 4:
+                    pictureBox1.Image = FakeFPS.Properties.Resources._4;
+                    break;
+                case 5:
+                    pictureBox1.Image = FakeFPS.Properties.Resources._5;
+                    break;
+                case 6:
+                    pictureBox1.Image = FakeFPS.Properties.Resources._6;
+                    break;
+                case 7:
+                    pictureBox1.Image = FakeFPS.Properties.Resources._7;
+                    break;
+                case 8:
+                    pictureBox1.Image = FakeFPS.Properties.Resources._8;
+                    break;
+                case 9:
+                    pictureBox1.Image = FakeFPS.Properties.Resources._9;
+                    break;
+            }
+        }
+
+        public void thousandMethod(int i)
+        {
+            switch (i)
+            {
+                case 0:
+                    pictureBox4.Image = FakeFPS.Properties.Resources._0;
+                    break;
+                case 1:
+                    pictureBox4.Image = FakeFPS.Properties.Resources._1;
+                    break;
+                case 2:
+                    pictureBox4.Image = FakeFPS.Properties.Resources._2;
+                    break;
+                case 3:
+                    pictureBox4.Image = FakeFPS.Properties.Resources._3;
+                    break;
+                case 4:
+                    pictureBox4.Image = FakeFPS.Properties.Resources._4;
+                    break;
+                case 5:
+                    pictureBox4.Image = FakeFPS.Properties.Resources._5;
+                    break;
+                case 6:
+                    pictureBox4.Image = FakeFPS.Properties.Resources._6;
+                    break;
+                case 7:
+                    pictureBox4.Image = FakeFPS.Properties.Resources._7;
+                    break;
+                case 8:
+                    pictureBox4.Image = FakeFPS.Properties.Resources._8;
+                    break;
+                case 9:
+                    pictureBox4.Image = FakeFPS.Properties.Resources._9;
+                    break;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            i++;
+                fps = rand.Next(a, b);
+                fpsS = fps.ToString();
+                //hundred = fpsS[0];
+                if (fpsS.Length == 4)
+                {
+                    thousand = fpsS[0];
+                    hundred = fpsS[1];
+                    ten = fpsS[2];
+                    one = fpsS[3];
+                    thousandMethod(Convert.ToInt32(thousand.ToString()));
+                    hundredMethod(Convert.ToInt32(hundred.ToString()));
+                    tenMethod(Convert.ToInt32(ten.ToString()));
+                    oneMethod(Convert.ToInt32(one.ToString()));
+                }
+                else if (fpsS.Length == 3)
+                {
+                    hundred = fpsS[0];
+                    ten = fpsS[1]; 
+                    one = fpsS[2];
+                    hundredMethod(Convert.ToInt32(hundred.ToString()));
+                    tenMethod(Convert.ToInt32(ten.ToString()));
+                    oneMethod(Convert.ToInt32(one.ToString()));
+                }
+                else if (fpsS.Length == 2) 
+                {
+                    ten = fpsS[0]; 
+                    one = fpsS[1];
+                    tenMethod(Convert.ToInt32(ten.ToString()));
+                    oneMethod(Convert.ToInt32(one.ToString()));
+                }
+                else 
+                { 
+                    one = fpsS[0]; 
+                    oneMethod(Convert.ToInt32(one.ToString())); 
+                };
+                if (i == count) { timer1.Enabled = false; panel1.Location = new Point(2, 378); count = 0; i = 0;}
+        }
+
+   }
+}
